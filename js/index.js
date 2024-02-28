5e326ae582b80a0ec2c976c89247e1b0f6c1388f
Bastien VILLEGAS <95182085+Bastien64@users.noreply.github.com>
2024-02-29 00:11:36 +0100

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+
+//SITE BOUTON SCROLL TO TOP
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
@@ -12,3 +14,22 @@ function scrollToTop() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
+
+
+//EFFET ZOOM IMAGE
+       // Fonction pour activer l'animation lorsque l'élément est visible dans la vue
+       function animateOnScroll(entries, observer) {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('zoomIn');
+                observer.unobserve(entry.target); // Arrête d'observer une fois que l'élément est visible
+            }
+        });
+    }
+
+    // Crée un observateur pour surveiller l'image
+    const observer = new IntersectionObserver(animateOnScroll, { root: null, threshold: 0.5 });
+
+    // Cible l'image et commence à observer
+    const image = document.getElementById('carbonImage');
+    observer.observe(image);
