a4f1489756a49a1144f348683adde38839c71940
Bastien VILLEGAS <95182085+Bastien64@users.noreply.github.com>
2024-03-03 15:13:23 +0100

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -40,3 +40,59 @@ function scrollToTop() {
         var divElement = document.querySelector(".divfooterbottom div");
         divElement.classList.toggle("visible"); // Toggle entre ajouter ou supprimer la classe visible
     });
+
+    
+  //COOKIES
+  // Vérifie si le cookie d'acceptation existe
+  function checkCookie() {
+    var accepted = getCookie("cookiesAccepted");
+    if (accepted) {
+      // Si le cookie existe, masquer la boîte de dialogue
+      document.querySelector('.container').style.display = 'none';
+    }
+  }
+
+  // Fonction pour récupérer un cookie par son nom
+  function getCookie(name) {
+    var cookies = document.cookie.split(';');
+    for (var i = 0; i < cookies.length; i++) {
+      var cookie = cookies[i].trim();
+      if (cookie.startsWith(name + '=')) {
+        return cookie.substring(name.length + 1);
+      }
+    }
+    return null;
+  }
+
+  // Fonction pour définir un cookie avec une durée d'expiration
+  function setCookie(name, value, days) {
+    var date = new Date();
+    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+    var expires = "expires=" + date.toUTCString();
+    document.cookie = name + "=" + value + ";" + expires + ";path=/";
+  }
+  // Vérifier si le cookie d'acceptation existe lors du chargement de la page
+  window.onload = checkCookie;
+  // Sélectionnez le bouton "close"
+  var closeButton = document.querySelector('.close');
+
+  // Ajoutez un gestionnaire d'événements pour le clic sur le bouton "close"
+  closeButton.addEventListener('click', function() {
+    // Sélectionnez le conteneur parent
+    var container = document.querySelector('.container');
+    // Masquez le conteneur parent
+    container.style.display = 'none';
+  });
+
+  // Sélectionnez le bouton "accept"
+  var acceptButton = document.querySelector('.accept');
+
+  // Ajoutez un gestionnaire d'événements pour le clic sur le bouton "accept"
+  acceptButton.addEventListener('click', function() {
+    // Définir le cookie d'acceptation pour une durée de 30 jours
+    setCookie("cookiesAccepted", "true", 30);
+    // Masquer la boîte de dialogue
+    document.querySelector('.container').style.display = 'none';
+  });
+
+
